4f642441e43db8e782c4b0da719f66272a021d82
damie <damie@192.168.2.94>
2019-08-02 16:56:35 +0200

    dap server v2

---
+++ src/main/java/fr/houseofcode/dap/server/tda/data/AppUserRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.houseofcode.dap.server.tda.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author damie
+ *
+ */
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+
+}
